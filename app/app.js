b85667766781a3fa2100ef40ef16c65684b9e3d8
Andrey P <pfm@live.ru>
2015-12-13 20:50:55 +0300

    Add Routing

--- app/app.js
+++ app/app.js
@@ -1,5 +1,19 @@
 var app;
 (function (app) {
-    angular
-        .module("productManagement", ["common.services", "productResourceMock"]);
+    var main = angular
+        .module("productManagement", ["ngRoute", "common.services", "productResourceMock"]);
+    main.config(routeConfig);
+    routeConfig.$inject = ["$routeProvider"];
+    function routeConfig($routeProvider) {
+        $routeProvider
+            .when("/productList", {
+            templateUrl: "/app/products/productListView.html",
+            controller: "productListCtrl as vm"
+        })
+            .when("/productDetail/:productId", {
+            templateUrl: "/app/products/productDetailView.html",
+            controller: "productDetailCtrl as vm"
+        })
+            .otherwise("/productList");
+    }
 })(app || (app = {}));
--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,22 @@
 module app {
-	angular
-	.module("productManagement", ["common.services", "productResourceMock"]);
+	var main = angular
+		.module("productManagement", ["ngRoute", "common.services", "productResourceMock"]);
+
+	main.config(routeConfig);
+
+	routeConfig.$inject = ["$routeProvider"];
+	function routeConfig($routeProvider: ng.route.IRouteProvider): void {
+
+		$routeProvider
+			.when("/productList", {
+				templateUrl: "/app/products/productListView.html",
+				controller: "productListCtrl as vm"
+			})
+			.when("/productDetail/:productId", {
+				templateUrl: "/app/products/productDetailView.html",
+				controller: "productDetailCtrl as vm"
+			})
+			.otherwise("/productList");
+	}
+
 }
